007bf158cf96bfd48b6cc32fdd730d940f7df588
stack-alchemy <evans.josephlouis@gmail.com>
2025-08-05 10:43:36 -0500

    update config file

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,10 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: ".env" });
 
-export const PRIVATE_KEY: string = process.env.PRIVATE_KEY || '';
-export const RPC_ENDPOINT: string = process.env.RPC_ENDPOINT || '';
-export const WS_ENDPOINT: string = process.env.WS_ENDPOINT || '';
-export const TARGET_WALLET: string = process.env.TARGET_WALLET || '';
+export const PRIVATE_KEY: string = process.env.PRIVATE_KEY || "";
+export const RPC_ENDPOINT: string = process.env.RPC_ENDPOINT || "";
+export const WS_ENDPOINT: string = process.env.WS_ENDPOINT || "";
+export const TARGET_WALLET: string = process.env.TARGET_WALLET || "";
+
+console.log(PRIVATE_KEY)
